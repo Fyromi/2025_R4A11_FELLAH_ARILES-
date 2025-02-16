2750f4017cf867e8a6d711f0ef610baa6fa0594e
Ariles Fellah <ariles.fellah937@gmail.com>
2025-02-16 01:14:03 +0100

    setUpMaster

--- lib/core/constants/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const String title = 'Compteur App';
-const Color primaryColor = Colors.red;
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tp1/viewmodels/CounterViewModel.dart';
-import 'package:tp1/views/HomeView.dart';
-import 'package:tp1/views/Page2View.dart';
-
-import 'core/constants/constants.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: HomePage()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+
+  int compteur = 0;
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (_) => CounterViewModel(),
-        child: MaterialApp(
-            title: title,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-              useMaterial3: true,
-            ),
-            initialRoute: '/mainPage',
-            routes: {
-              '/secondPage': (context) => Page2View(),
-              '/mainPage': (context) => HomeView(),
-            }
-        )
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("HomePage"), elevation: 12),
+        body: Center(child: Text("Bonjour $compteur")),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                compteur += 1;
+              });
+            },
+            child: Icon(Icons.add)));
   }
 }
